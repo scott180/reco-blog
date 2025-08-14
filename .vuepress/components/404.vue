3913ab02ac5fbdbcb833adad7fedeea414b28f31
xuyq <xuyongqin@hoomi.onaliyun.com>
2025-08-14 16:19:07 +0800

    fix reco version

---
+++ .vuepress/components/404.vue
@@ -0,0 +1,15 @@
+<!-- 404.vue -->
+<template>
+  <div class="theme-reco-404">
+    <h1>404 - 页面未找到</h1>
+    <p>你访问的页面不存在。</p>
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<style>
+.theme-reco-404 {
+  text-align: center;
+  padding: 100px 20px;
+}
+</style>
